c3a2d094b94408341400b3a70bff2b02c54c25e7
mydevcave <michalwalczakpoczta@gmail.com>
2025-03-28 12:54:55 +0100

    test setup

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,27 @@
+describe('Category A Tests', () => {
+    it('Test A1 - dummy test', () => {
+        cy.log('Running Test A1');
+    });
+
+    it('Test A2 - dummy test', () => {
+        cy.log('Running Test A2');
+    });
+
+    it('Test A3 - dummy test', () => {
+        cy.log('Running Test A3');
+    });
+});
+
+describe('Category B Tests', () => {
+    it('Test B1 - dummy test', () => {
+        cy.log('Running Test B1');
+    });
+
+    it('Test B2 - dummy test', () => {
+        cy.log('Running Test B2');
+    });
+
+    it('Test B3 - dummy test', () => {
+        cy.log('Running Test B3');
+    });
+});
